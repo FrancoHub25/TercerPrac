0c197e198cc3246507059f49982a8c3952e6e049
FrancoHub25 <francoretamozo25@gmail.com>
2022-10-03 00:26:02 -0500

    first

---
+++ src/main/java/com/practicas/emp/PracticasTercApplication.java
@@ -0,0 +1,13 @@
+package com.practicas.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasTercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasTercApplication.class, args);
+	}
+
+}
